ec57edd13305feb6f719e4c4dc88e7b1524cd6cd
XinNie <umnie2@myumanitoba.ca>
2024-10-30 13:49:07 -0500

    add hr between tabs, add lat update info in sidebar for each note

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 
 export default function Sidebar(props) {
+  function timeConverter(note) {
+    const date = note.updatedAt.toDate();
+    const formattedDate = `${date.getFullYear()}-${String(
+      date.getMonth() + 1
+    ).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")} ${String(
+      date.getHours()
+    ).padStart(2, "0")}:${String(date.getMinutes()).padStart(2, "0")}`;
+
+    return `Last updated: ${formattedDate}`;
+  }
+
   const noteElements = props.notes.map((note, index) => (
     <div key={note.id}>
       <div
@@ -9,7 +20,10 @@ export default function Sidebar(props) {
         }`}
         onClick={() => props.setCurrentNoteId(note.id)}
       >
-        <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
+        <div className="sidbar-display">
+          <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
+          <h6 className="text-snippet-time">{timeConverter(note)}</h6>
+        </div>
         <button
           className="delete-btn"
           onClick={(event) => props.deleteNote(note.id)}
@@ -17,6 +31,7 @@ export default function Sidebar(props) {
           <i className="gg-trash trash-icon"></i>
         </button>
       </div>
+      <hr />
     </div>
   ));
 
